114823aa5b6302126ff54fbe82799906d49aacec
Tushil_G <t.gunness35@gmail.com>
2023-05-06 10:24:00 +0400

    feat google search,youtube search and meme

--- jarvisWeb/main.js
+++ jarvisWeb/main.js
@@ -5,9 +5,9 @@ const  speakBtn = document.querySelector("#speak");
 
 
 //speech recongnition setup
-const SpeechRecognition = window.speechRecognition || window.webkitSpeechRecognition;
+let SpeechRecognition = window.speechRecognition || window.webkitSpeechRecognition;
 // console.log(new SpeechRecognition());
-const recognition = new SpeechRecognition()
+let recognition = new SpeechRecognition()
 // start
 recognition.onstart = function() {
     console.log('Voice is activated');
@@ -18,11 +18,36 @@ startBtn.addEventListener("click", ()=>{
 
 //result
 recognition.onresult = function(event){
-    console.log(event)
-    const current = event.resultIndex;
-    const transcript = event.results[current][0].transcript;
-    console.log(transcript)
-    readOut(transcript)
+    // console.log(event)
+    let current = event.resultIndex;
+    let transcript = event.results[current][0].transcript;
+    transcript=transcript.toLowerCase();
+    console.log(`words ${transcript}`)
+    // readOut(transcript)
+    if (transcript.includes("hi jarvis")||transcript.includes("hello jarvis")||transcript.includes("hey jarvis")||transcript.includes("jarvis")||transcript.includes("hi")||transcript.includes("hello")||transcript.includes("hey")||transcript.includes("hi jarvis how are you")||transcript.includes("hello jarvis how are you")||transcript.includes("hey jarvis how are you")||transcript.includes("jarvis how are you")){
+        readOut("hello bro")
+    }
+    /*
+    please don't remove this code
+    !important
+    */
+    if (transcript.includes("open youtube")||transcript.includes("youtube")){
+        readOut("opening youtube")
+        // (method) open(url?: string | URL 
+        window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ",'_blank');
+    }
+    if (transcript.includes(`search`)){
+        let search = transcript.replace("search", "")
+        readOut(`searching ${search}`)
+        // search = search.replace(" ", "+")
+        window.open('https://www.google.com/search?q=' + search,'_blank');
+    }
+    if (transcript.includes("play")){
+        let search = transcript.replace("play", "")
+        readOut(`playing ${search}`)
+        window.open('https://www.youtube.com/results?search_query=' + search,'_blank');
+    }
+
 };
 //stop
 recognition.onend = function(){
@@ -37,7 +62,7 @@ recognition.continuous = true;
 
 // speech
 function readOut(message){
-    const speech = new SpeechSynthesisUtterance()
+    let speech = new SpeechSynthesisUtterance();
     //different voices
     const allvoice = speechSynthesis.getVoices();
     speech.voice = allvoice[1]
@@ -48,7 +73,7 @@ function readOut(message){
 }
 // some kind of example to see if it works
 speakBtn.addEventListener("click", ()=>{
-    readOut("hello sir")
+    readOut("hello bro")
 })
 
 // window.onload = function(){
