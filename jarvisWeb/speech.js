df7be721128fc9ef3e39b56c3e422b7cdb283b70
Tushil_G <t.gunness35@gmail.com>
2023-05-07 11:52:48 +0400

    feat weather/jokes removed:github profile

---
+++ jarvisWeb/speech.js
@@ -0,0 +1,91 @@
+//elements
+const  startBtn = document.querySelector("#start");
+const  stopBtn = document.querySelector("#stop");
+const  speakBtn = document.querySelector("#speak");
+
+// speech
+function readOut(message){
+    let speech = new SpeechSynthesisUtterance();
+    //different voices
+    const allvoice = speechSynthesis.getVoices();
+    speech.voice = allvoice[1]
+    speech.text = message;
+    speech.volume = 1;
+    window.speechSynthesis.speak(speech)
+    console.log("speaking out")
+}
+//speech recongnition setup
+let SpeechRecognition = window.speechRecognition || window.webkitSpeechRecognition;
+// console.log(new SpeechRecognition());
+let recognition = new SpeechRecognition()
+// start
+recognition.onstart = function() {
+    console.log('Voice is activated');
+};
+startBtn.addEventListener("click", ()=>{
+    recognition.start()
+})
+
+//result
+recognition.onresult = function(event){
+    // console.log(event)
+    let current = event.resultIndex;
+    let transcript = event.results[current][0].transcript;
+    transcript=transcript.toLowerCase();
+    let userdata = localStorage.getItem("jarvis_setup")
+    console.log(`words ${transcript}`)
+    // readOut(transcript)
+    if (transcript.includes("hi jarvis")||transcript.includes("hello jarvis")||transcript.includes("hey jarvis")||transcript.includes("jarvis")||transcript.includes("hi")||transcript.includes("hello")||transcript.includes("hey")||transcript.includes("hi jarvis how are you")||transcript.includes("hello jarvis how are you")||transcript.includes("hey jarvis how are you")||transcript.includes("jarvis how are you")){
+        readOut("hello sir")
+    }
+    if (transcript.includes("rick")||transcript.includes("roll")||transcript.includes("rick roll")){
+        readOut("opening youtube")
+        // (method) open(url?: string | URL
+        window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ",'_blank');
+    }
+    if (transcript.includes(`search`)){
+        let search = transcript.replace("search", "")
+        readOut(`searching ${search}`)
+        // search = search.replace(" ", "+")
+        window.open('https://www.google.com/search?q=' + search,'_blank');
+    }
+    if (transcript.includes("play")){
+        let search = transcript.replace("play", "")
+        readOut(`playing ${search}`)
+        window.open('https://www.youtube.com/results?search_query=' + search,'_blank');
+    }
+    if (transcript.includes("wiki")){
+        let search = transcript.replace("wiki", "")
+        readOut(`searching ${search}`)
+        window.open('https://en.wikipedia.org/wiki/A/search=' + search,'_blank');
+    }    
+    if(transcript.includes("joke")){
+        fetch("https://official-joke-api.appspot.com/random_joke")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            readOut(data.setup)
+            setTimeout(() => {
+                readOut(data.punchline)
+            }, 3000);
+        })
+    }
+//stop
+recognition.onend = function(){
+    console.log('voice is deactivated');
+};
+stopBtn.addEventListener("click", ()=>{
+    recognition.stop()
+})
+
+// continuos
+recognition.continuous = true;
+
+// some kind of example to see if it works
+speakBtn.addEventListener("click", ()=>{
+    readOut("hello bro")
+})}
+
+// window.onload = function(){
+//     readOut("    ");
+// };
---
+++ jarvisWeb/weather.js
@@ -0,0 +1,60 @@
+getLocation();
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        alert("Geolocation is not supported by this browser.");
+        document.getElementById("body").style.filter = 'blur(0rem)';
+    }
+}
+
+
+
+function showPosition(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    getName(latitude, longitude);
+}
+async function getName(latitude, longitude) {
+    var access_key = '359de5c3bc8174d9c1a8d40311ce2425'
+    var url = `https://api.openweathermap.org/geo/1.0/reverse?lat=` + latitude + `&lon=` + longitude + `&limit=5&appid=` + access_key;
+    var res = await fetch(url);
+    var data = await res.json();
+    var location = data[0].name.split(" ")[0];
+    fetchData(location);    
+}
+
+function findWeather() {
+    var location = document.getElementById("search").value;
+    fetchData(location)
+}
+async function fetchData(location) {
+    var url = `https://api.weatherapi.com/v1/current.json?key=1c63857a8e0548f9a1a152750210909&q=` + location + `&aqi=yes`;
+    var res = await fetch(url);
+    var data = await res.json();
+    if(res.status == 200){
+        setValues(data);
+    }
+    else{
+        var tags = document.getElementsByClassName('reset');
+        for (let index = 0; index < tags.length; index++) {
+            tags.item(index).innerHTML = ""
+        }
+        document.getElementById("name").innerHTML = "Location not Found";
+    }
+}
+function setValues(data) {
+    var setter = document.getElementById("temp_c");
+    setter.innerHTML = data.current.temp_c + `&#176`;
+    
+    setter = document.getElementById('name');
+    setter.innerHTML = data.location.name;
+
+    setter = document.getElementById("region");
+    setter.innerHTML = `&nbsp;` +  data.location.region + `, ` + data.location.country;
+    setter = document.getElementById("feelslike_c");
+    setter.innerHTML = `Feels like ` + data.current.feelslike_c + `&#176`;
+
+    setter = document.getElementById("condition");
+    setter.innerHTML = data.current.condition.text;
+}
