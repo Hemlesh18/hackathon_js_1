a8c2d70ff5624bd5e2564e945dc10b9b35eb70b2
Hemlesh18 <hemlesh@icloud.com>
2023-05-07 19:53:14 +0400

    time,battrey,internet connectivity

--- jarvisWeb/speech.js
+++ jarvisWeb/speech.js
@@ -3,7 +3,39 @@ const  startBtn = document.querySelector("#start");
 const  stopBtn = document.querySelector("#stop");
 const  speakBtn = document.querySelector("#speak");
 
+// jarvis setup
+if (localStorage.getItem("jarvis_setup")!== null){
+    // weather()
+}
+// jarvis information set up
+const setup = document.querySelector(".jarvis_setup")
+setup.style.display="none"
+if (localStorage.getItem("jarvis_setup") === null){
+    setup.style.display="block"
+    setup.querySelector("button").addEventListener("click", userInfo)
+}
+// userinfo function
+function userInfo(){
+    let setupInfo = {
+        name: setup.querySelectorAll("input")[0].value,
+        location: setup.querySelectorAll("input")[1].value,
+        github: setup.querySelectorAll("input")[2].value,
+        }
 
+        let testArr=[]
+        setup.querySelectorAll("input").forEach((e)=>{
+            testArr.push(e.value)
+        })
+        if(testArr.includes("")){
+            readOut("sir enter your complete infomation")
+        }
+        else{
+            localStorage.clear()
+            localStorage.setItem("jarvis_setup", JSON.stringify(setupInfo))
+            setup.style.display="none"
+            weather(JSON.parse(localStorage.getItem("jarvis_setup")).location)
+        }
+    }
 // speech
 function readOut(message){
     let speech = new SpeechSynthesisUtterance();
@@ -60,7 +92,7 @@ recognition.onresult = function(event){
         let search = transcript.replace("wiki", "")
         readOut(`searching ${search}`)
         window.open('https://en.wikipedia.org/wiki/A/search=' + search,'_blank');
-    }    
+    }
     if(transcript.includes("joke")){
         fetch("https://official-joke-api.appspot.com/random_joke")
         .then(response => response.json())
@@ -92,3 +124,78 @@ speakBtn.addEventListener("click", ()=>{
 // window.onload = function(){
 //     readOut("    ");
 // };
+
+
+// date and time
+let date = new Date();
+let hrs = date.getHours();
+let mins = date.getMinutes();
+let secs = date.getSeconds();
+
+function formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    currentTime = strTime
+    time.textContent = strTime
+  }
+
+  formatAMPM(date)
+  setInterval(() => {
+    formatAMPM(date)
+  }, 60000);
+
+  // auto friday
+
+  function autoJarvis() {
+    setTimeout(() => {
+      recognition.start();
+    }, 1000);
+  }
+  // battery
+  let batteryPromise = navigator.getBattery();
+  batteryPromise.then(batteryCallback);
+
+  // internet connectivity
+
+    if(navigator.onLine){
+      document.querySelector("#internet").textContent = "online"
+      connectivity = "online"
+    } else {
+      document.querySelector("#internet").textContent = "offline"
+      connectivity = "offline"
+    }
+
+  setInterval(() => {
+    if(navigator.onLine){
+      document.querySelector("#internet").textContent = "online"
+      connectivity = "online"
+    } else {
+      document.querySelector("#internet").textContent = "offline"
+      connectivity = "offline"
+    }
+  }, 60000);
+
+  function batteryCallback(batteryObject) {
+    printBatteryStatus(batteryObject);
+    setInterval(() => {
+      printBatteryStatus(batteryObject);
+    }, 5000);
+  }
+  function printBatteryStatus(batteryObject) {
+    document.querySelector("#battery").textContent = `${
+      (batteryObject.level * 100).toFixed(2)
+    }%`;
+    charge = batteryObject.level * 100
+    if (batteryObject.charging === true) {
+      document.querySelector(".battery").style.width = "200px";
+      document.querySelector("#battery").textContent = `${
+        (batteryObject.level * 100).toFixed(2)
+      }% Charging`;
+      chargeStatus = "plugged in"
+    }
+  }
